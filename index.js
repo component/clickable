e8bded1adecbed6349f613d3e13b17910c989311
Jonathan Ong <jonathanrichardong@gmail.com>
2013-11-29 20:59:16 -0800

    button vs touch support

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 module.exports = function (e) {
-  // touch support
-  if (e.type !== 'click')
-    return true
+  // primary button only
+  if (e.type === 'click' && e.button !== 0)
+    return false
+  // one touch
+  if (e.touches && e.touches.length > 1)
+    return false
 
-  return !(e.button !== 0
-    || e.defaultPrevented
+  return !(e.defaultPrevented
     || e.ctrlKey
     || e.altKey
     || e.shiftKey
